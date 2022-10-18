bb3a8778177b8680fddf105fdf1c464df5ec49d6
Krestian Weken <chrisk8er@gmail.com>
2022-10-18 16:05:15 +0800

    optimize picture

--- src/components/message-list.tsx
+++ src/components/message-list.tsx
@@ -10,63 +10,65 @@ export const MessageList = () => {
   const [showModal, setShowModal] = React.useState(false);
 
   return (
-    <div
-      sx={{
-        overflowY: 'auto',
-        paddingRight: `1rem`,
-        position: 'relative',
-      }}
-    >
+    <React.Fragment>
       <Button
-        sx={{ width: `100%` }}
+        sx={{ width: `100%`, height: 'fit-content' }}
         variant="primary"
         onClick={() => setShowModal(true)}
       >
         Tulis Pesan
       </Button>
-      {data.map(({ id, name, attandence, message }) => (
-        <div
-          sx={{
-            marginTop: `2rem`,
-            marginBottom: `2rem`,
-            backgroundColor: 'rgba(218, 223, 233, 0.31)',
-            padding: '1rem',
-            borderRadius: '20px',
-          }}
-          key={id}
-        >
-          <h4 sx={{ margin: `3px 0` }}>{name}</h4>
-          <p sx={{ margin: `3px 0` }}>
-            <small>
-              <strong>{attandence}</strong>
-            </small>
-          </p>
-          <p sx={{ margin: `3px 0` }}>{message}</p>
-        </div>
-      ))}
-
-      <ReactModal
-        isOpen={showModal}
-        ariaHideApp={false}
-        style={{
-          content: { width: '100vw', height: '100vh', top: 0, left: 0 },
+      <div
+        sx={{
+          overflowY: 'auto',
+          paddingRight: `1rem`,
+          position: 'relative',
         }}
       >
-        <div sx={{ display: 'flex', flexDirection: 'row-reverse' }}>
-          <span
-            sx={{ textAlign: 'end', fontSize: 'xx-large' }}
-            onClick={() => setShowModal(false)}
+        {data.map(({ id, name, attandence, message }) => (
+          <div
+            sx={{
+              marginTop: `2rem`,
+              marginBottom: `2rem`,
+              backgroundColor: 'rgba(218, 223, 233, 0.31)',
+              padding: '1rem',
+              borderRadius: '20px',
+            }}
+            key={id}
           >
-            &times;
-          </span>
-        </div>
-        <MessageForm
-          onSending={(data) => {
-            setShowModal(false);
-            refetch();
+            <h4 sx={{ margin: `3px 0` }}>{name}</h4>
+            <p sx={{ margin: `3px 0` }}>
+              <small>
+                <strong>{attandence}</strong>
+              </small>
+            </p>
+            <p sx={{ margin: `3px 0` }}>{message}</p>
+          </div>
+        ))}
+
+        <ReactModal
+          isOpen={showModal}
+          ariaHideApp={false}
+          style={{
+            content: { width: '100vw', height: '100vh', top: 0, left: 0 },
           }}
-        />
-      </ReactModal>
-    </div>
+        >
+          <div sx={{ display: 'flex', flexDirection: 'row-reverse' }}>
+            <span
+              sx={{ textAlign: 'end', fontSize: 'xx-large' }}
+              onClick={() => setShowModal(false)}
+            >
+              &times;
+            </span>
+          </div>
+          <MessageForm
+            onSending={(data) => {
+              setShowModal(false);
+              refetch();
+            }}
+          />
+        </ReactModal>
+      </div>
+    </React.Fragment>
   );
 };
